a974e25bc609898b0045622152410b577810c8d9
liquidz <liquidz.uo@gmail.com>
2020-04-25 07:09:12 +0900

    update

--- entrypoint.clj
+++ entrypoint.clj
@@ -1,5 +1,27 @@
 #!/usr/bin/env bb
 
+(defn- file->ns-name
+  [root-file file]
+  (let [relpath (subs (.getAbsolutePath file)
+                      (inc (count (.getAbsolutePath root-file))))
+        sep (System/getProperty "file.separator")]
+    (-> relpath
+        (str/replace #"\.clj$" "")
+        (str/replace #"_" "-")
+        (.replace sep "."))))
+
+(defn- list-files
+  [file]
+  (->> (seq (.listFiles file))
+       (mapcat #(if (.isFile %) [%] (list-files %)))))
+
+(defn- list-test-namespaces
+  [root-path test-file-re]
+  (let [root (io/file root-path)]
+    (->> (list-files root)
+         (filter #(re-seq test-file-re (.getName %)))
+         (map #(file->ns-name root %)))))
+
 (defn- test-code
   [namespaces]
   `(let [ns-syms# (map symbol ~namespaces)]
@@ -7,17 +29,22 @@
        (require (symbol x#)))
 
      (let [res# (apply clojure.test/run-tests ns-syms#)]
-       ;(println (:out res#))
        (System/exit (+ (:fail res#) (:error res#))))))
 
-(when (not= 2 (count *command-line-args*))
-  (println "FIXME")
-  (System/exit 1))
+(defn -main
+  [[source-paths test-paths test-file-pattern]]
+  (let [source-paths (str/split source-paths #" ")
+        test-paths (str/split test-paths #" ")
+        test-file-re (re-pattern test-file-pattern)
 
-(let [[classpaths namespaces] *command-line-args*
-      classpaths (str/replace classpaths #" " ":")
-      namespaces (str/split namespaces #" ")
-      {:keys [exit out]} (apply shell/sh ["bb" "--classpath" classpaths
-                                          (str (test-code namespaces))])]
-  (println out)
-  (System/exit exit))
+        classpaths (str/join ":" (concat source-paths test-paths))
+        namespaces (vec (mapcat #(list-test-namespaces % test-file-re) test-paths))
+        {:keys [exit out]} (apply shell/sh ["bb" "--classpath" classpaths
+                                            (str (test-code namespaces))])]
+    (println out)
+    (System/exit exit)))
+
+(if (not= 3 (count *command-line-args*))
+  (do (println "Usage: ./entrypoint.clj 'src-paths' 'test-patsh' 'test-file-pattern'")
+      (System/exit 1))
+  (-main *command-line-args*))
