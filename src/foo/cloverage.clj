2a68eb4674ca622100c2cd3f18975bee3f017093
liquidz <liquidz.uo@gmail.com>
2022-10-14 10:14:38 +0900

    wip

--- src/foo/cloverage.clj
+++ src/foo/cloverage.clj
@@ -6,8 +6,12 @@
   "cloverage のカスタムレポーター
   サマリの合計値のみを GitHub Actions の Output として出力する"
   [{:keys [forms]}]
-  (let [totalled-stats (report/total-stats forms)]
+  (let [totalled-stats (report/total-stats forms)
+        github-output (System/getenv "GITHUB_OUTPUT")]
     (println (System/getenv "HOGE"))
     (println (System/getenv "GITHUB_OUTPUT"))
-    (println (format "::set-output name=forms::%.2f%%" (:percent-forms-covered totalled-stats)))
-    (println (format "::set-output name=lines::%.2f%%" (:percent-lines-covered totalled-stats)))))
+    ;; (println (format "::set-output name=forms::%.2f%%" (:percent-forms-covered totalled-stats)))
+    ;; (println (format "::set-output name=lines::%.2f%%" (:percent-lines-covered totalled-stats)))))
+    (when github-output
+      (spit github-output (format "forms=%.2f%%\n" (:percent-forms-covered totalled-stats)) :append true)
+      (spit github-output (format "lines=%.2f%%\n" (:percent-lines-covered totalled-stats)) :append true))))
